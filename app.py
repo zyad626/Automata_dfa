3f89ce689d53787619e09b17977a6455c3871b86
zyad yasser <zyadyasser626@gmail.com>
2022-05-15 14:08:10 +0200

    added a before running instructions

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+#BEFORE RUNNING:
+#1. install graphviz and add it to path: https://graphviz.org/download/#:~:text=Windows%20install%20packages%3A-,graphviz%2D3.0.0,-graphviz%2D3.0.0%20(32
+#2. install visual_automata: pip install visual-automata
+#3. restart vscode or the terminal that you are using
+
 from dfa_class import my_DFA
 from automata.fa.dfa import DFA
 from visual_automata.fa.dfa import VisualDFA
